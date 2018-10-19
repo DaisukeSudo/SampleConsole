b7be894bfe9e52caadedd75525e60aa0db1d4541
dsudo <dsudo0624@gmail.com>
2018-10-20 01:55:23 +0900

    Add Extensions

---
+++ SampleConsole/Extensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleConsole
+{
+    public static class Extensions
+    {
+        public static string ToJson<T>(this IEnumerable<T>  list) => $"[{string.Join(", ", list)}]";
+    }
+}
--- SampleConsole/Sample2/Case1/Caller.cs
+++ SampleConsole/Sample2/Case1/Caller.cs
@@ -15,16 +15,16 @@ namespace SampleConsole.Sample2.Case1
 
             var list1 = new int[] { 0, 1, 2, 3, 4, 5, 6 };
             var list2 = new int[] { 1, 3, 5 };
-            Console.WriteLine($"list1 = [{string.Join(", ", list1)}]");
-            Console.WriteLine($"list2 = [{string.Join(", ", list2)}]");
+            Console.WriteLine($"list1 = {list1.ToJson()}");
+            Console.WriteLine($"list2 = {list2.ToJson()}");
             Console.WriteLine();
 
             Console.WriteLine("Ignore(list1, list2)");
-            Console.WriteLine($"[{string.Join(", ", Ignore(list1, list2))}]");
+            Console.WriteLine(Ignore(list1, list2).ToJson());
             Console.WriteLine();
 
             Console.WriteLine("Swap(list1, 2, 5)");
-            Console.WriteLine($"[{string.Join(", ", Swap(list1, 2, 5))}]");
+            Console.WriteLine(Swap(list1, 2, 5).ToJson());
             Console.WriteLine();
         }
     }
