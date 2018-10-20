77efa60bdae64bb9cdd8c4d1ff8accd4d2e33013
dsudo <dsudo0624@gmail.com>
2018-10-20 12:13:06 +0900

    Add Product

---
+++ SampleConsole/Sample2/Case2/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleConsole.Sample2.Case2
+{
+    public class Product
+    {
+        private string productCode;
+        private string productName;
+        private Decimal price;
+
+        public Product(
+            string productCode,
+            string productName,
+            Decimal price)
+        {
+            this.productCode = productCode;
+            this.productName = productName;
+            this.price = price;
+        }
+
+        public string getProductCode() => this.productCode;
+        public string getProductName() => this.productName;
+        public Decimal getPrice() => this.price;
+    }
+}
